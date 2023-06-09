691135f75549497c2bc8859afc4e7aeb1a4ef41e
Michela30 <micheladestefano30@gmail.com>
2023-06-09 15:00:08 +0200

    esercizio completato

---
+++ js/script.js
@@ -0,0 +1,23 @@
+
+const firstName = prompt ('Inserisci il tuo nome');
+
+console .log(firstName, typeof firstName);
+
+const lastName = prompt ('Inserisci il tuo cognome');
+
+console .log(lastName, typeof lastName);
+
+const favouriteColor = prompt ('Inserisci il tuo colore preferito');
+
+console .log(favouriteColor, typeof favouriteColor);
+
+document .writeln(`${firstName}
+${lastName}
+${favouriteColor}
+${101}`)
+
+
+document .getElementById('my-text').innerHTML = (`${firstName}
+${lastName}
+${favouriteColor}
+${101}`)
